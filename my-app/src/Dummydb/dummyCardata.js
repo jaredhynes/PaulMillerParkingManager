16985417cb0e94e8a46f7323106f1a595a798457
Mark-Malysa <marmal22@bergen.org>
2022-01-04 14:46:11 -0500

    Create dummyCardata.js

---
+++ my-app/src/Dummydb/dummyCardata.js
@@ -0,0 +1,42 @@
+model.export =[
+    {
+        key: "jdm485867483756",
+        make_model: "Nissan",
+        stockNum: "34",
+        newSpot: "a-1",
+        oldSpot: "N/A",
+        time: Date().toLocaleString()
+    },
+    {
+        key: "jdm57647354",
+        make_model: "Nissan",
+        stockNum: "34",
+        newSpot: "a-2",
+        oldSpot: "N/A",
+        time: Date().toLocaleString()
+    },
+    {
+        key: "jdm4856349769874389",
+        make_model: "Nissan",
+        stockNum: "34",
+        newSpot: "a-3",
+        oldSpot: "N/A",
+        time: Date().toLocaleString()
+    },
+    {
+        key: "jdm594u58433",
+        make_model: "Nissan",
+        stockNum: "34",
+        newSpot: "a-4",
+        oldSpot: "N/A",
+        time: Date().toLocaleString()
+    },
+    {
+        key: "jdm58732806734",
+        make_model: "Nissan",
+        stockNum: "34",
+        newSpot: "a-5",
+        oldSpot: "N/A",
+        time: Date().toLocaleString()
+    }
+]
