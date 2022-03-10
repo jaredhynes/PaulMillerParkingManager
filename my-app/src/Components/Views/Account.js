3a26e85ec68ebe844de79c26811d2a0c09868ca5
davkri22 <davkri22@bergen.org>
2022-03-10 14:15:51 -0500

    changed paths

--- my-app/src/Components/Views/Account.js
+++ my-app/src/Components/Views/Account.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "react-bootstrap/esm/Button";
+import LogoutButton from "../Login/LogoutButton";
 
 const Account = () => {
     const { logout } = useAuth0();
 
     return (
-        <Button onClick={() => logout({ returnTo: "http://localhost:3000" })}>
-            Log Out
-        </Button>
+        <LogoutButton></LogoutButton>
     );
 };
 
