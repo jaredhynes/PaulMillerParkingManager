d9146f66a87e081fe56afb5b7b5461dc2e70b97f
Mark-Malysa <marmal22@bergen.org>
2022-03-23 09:26:47 -0400

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,7 +17,9 @@ import { useAuth0 } from '@auth0/auth0-react';
 let edits = []
 
 
-const PATH = "https://gentle-thicket-28075.herokuapp.com/"
+//const PATH = "https://gentle-thicket-28075.herokuapp.com/"
+
+const PATH = "https://localhost:8001/"
 
 const App = () => {
 
