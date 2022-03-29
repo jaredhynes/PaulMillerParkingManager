e63ea3e5e2c6d6a50ac32d42d89147061faa5b13
davkri22 <davkri22@bergen.org>
2022-03-29 14:58:04 -0400

    details shows car history

--- my-app/src/Components/Views/CarPage.js
+++ my-app/src/Components/Views/CarPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { useParams } from "react-router";
+import TableOfEdits from "./TableOfEdits";
 
 function CarPage(props) {
     const { vin } = useParams(window.location.search);
@@ -13,6 +14,8 @@ function CarPage(props) {
             <h3>Stock Number: {car.stockNum}</h3>
             <h4>Location: {car.spot_name}</h4>
             <Button>Edit Information</Button>
+            <h4>Car History:</h4>
+            <TableOfEdits edits={props.edits.filter(edit => edit.car_id === vin)}/>
         </div>
     );
 }
