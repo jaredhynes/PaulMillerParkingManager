24cb77a1f97ad2fdff09c4e20648e7c90e19e1b9
Mark-Malysa <marmal22@bergen.org>
2022-03-30 08:40:24 -0400

    Update ParkingMap.js

--- my-app/src/Components/Views/ParkingMap.js
+++ my-app/src/Components/Views/ParkingMap.js
@@ -53,28 +53,6 @@ function ParkingMap(props) {
 
 	return (
 		<div className='mapImg'>
-			{/* <ResponsiveGridLayout
-				className="layout"
-				layouts ={layouts}
-				breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-				cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-				>
-					<div key="1">1</div>
-					<div key="2">2</div>
-					<div key="3">3</div>
-				</ResponsiveGridLayout> */}
-			{/* { <Container fluid>
-				{cars2d.map((row) => (
-					<Row debug>
-						{row.map((car) => (
-							<Col onClick={() => highlightCar(car)}>
-								<ShowOnHover car={car} />
-							</Col>
-						))}
-					</Row>
-				))}
-			</Container> } */}
-
 			<TransformWrapper>
 				<TransformComponent>
 					<img src={map} alt="test"/>
