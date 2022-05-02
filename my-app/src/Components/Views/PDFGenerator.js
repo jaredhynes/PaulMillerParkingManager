5f9a801f1d99250ce214e475098d945cf59987df
bdjared <jarhyn22@bergen.org>
2022-05-02 15:01:02 -0400

    New PDF Test

--- my-app/src/Components/Views/PDFGenerator.js
+++ my-app/src/Components/Views/PDFGenerator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Document, Page, Image, View, StyleSheet } from '@react-pdf/renderer';
-// import logo from '../../../public/audi.png';
+import logo from '../../../public/audi.png';
 
 
 function PDFGenerator(props) {
@@ -32,7 +32,7 @@ function PDFGenerator(props) {
     const { PDFImageIds } = props;
 
     const resultArray = PDFImageIds.map(id => {
-        // IdsArray.push(id);
+        IdsArray.push(id);
         return document.getElementById(id).toDataURL();
     })
 
@@ -44,12 +44,12 @@ function PDFGenerator(props) {
                         <View style={styles.view}>
                             <Image allowDangerousPaths src={dataURL} style={styles.QRImage} />    
                         </View>
-                        {/* <View style={styles.view}>
+                        { <View style={styles.view}>
                             <Text style={styles.text}>{IdsArray[id]}</Text>    
-                        </View> */}
-                        {/* <View style={styles.view}>
+                        </View> }
+                        { <View style={styles.view}>
                             <Image src={logo} style={styles.logoImage}/>    
-                        </View> */}
+                        </View> }
                     </Page>
                 );
             })}
