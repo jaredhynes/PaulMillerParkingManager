f98694a4f4ea091bb12ddb43e62d0f8937276351
Mark-Malysa <marmal22@bergen.org>
2022-05-09 20:00:09 -0400

    Update ParkingMap.js

--- my-app/src/Components/Views/ParkingMap.js
+++ my-app/src/Components/Views/ParkingMap.js
@@ -5,7 +5,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 // import map from "../../Images/prototype_map.png";
 import MapImage from "./MapImage.js";
-import { Button } from 'react-bootstrap';
+
 
 
 // import { Responsive, WidthProvider } from "react-grid-layout";
@@ -62,7 +62,6 @@ function ParkingMap(props) {
 				</TransformComponent>
 			</TransformWrapper>
 			{/* <MapImage/> */}
-			<Button onClick={() => console.log(data)}>Log Data</Button>
 		</div>
 		);
 
