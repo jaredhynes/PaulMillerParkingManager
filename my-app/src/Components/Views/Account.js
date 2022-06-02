83034118f77cd1e29805029949d0148161819d99
bdjared <jarhyn22@bergen.org>
2022-06-01 22:39:07 -0400

    Account settings page update

--- my-app/src/Components/Views/Account.js
+++ my-app/src/Components/Views/Account.js
@@ -1,13 +1,19 @@
 import React from "react";
 import LogoutButton from "../Login/LogoutButton";
+import '../../Styles/account.css';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap');
+</style>
 
 const Account = (props) => {
+    
     let data = props.data;
     return (
-        <div>
+        <div className="settings">
+            <h2><center>Hello, {data.user.name}</center></h2>
             <img src={data.user.picture} alt={data.user.name} />
-            <h2>{data.user.name}</h2>
-            <p>{data.user.email}</p>
+            <p><span className="info"><b>Name: </b></span>{data.user.name}</p>
+            <p><span className="info"><b>Email: </b></span>{data.user.email}</p>
             <LogoutButton data={data} />
         </div>
     );
